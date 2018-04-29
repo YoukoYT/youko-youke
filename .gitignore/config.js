ac39ddb525a6b5fccedb1c6a713e4f4ec3474e8a
YoukoYT <djillali.k@hotmail.com>
2018-04-29 14:45:35 +0200

    Update config.js

--- .gitignore/config.js
+++ .gitignore/config.js
@@ -1,5 +1,5 @@
 exports.TOKEN = 'NDM5MTQxNDc4MTQ5NjUyNDgw.DcO15Q.2G8yerRLXjMM5Z3dKdDP4LqqhPU';
 
-exports.PREFIX = '-';
+exports.PREFIX = '&';
 
 exports.GOOGLE_API_KEY = 'AIzaSyBCNMlyaRrxksWtcy-NNJE40Jd_wkmXbI0';
